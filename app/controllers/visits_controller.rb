5d41381654f3c08429773fcd70e208d60d305c44
Ilia Lobsanov <ilia@nurey.com>
2015-10-15 01:23:38 -0400

    Get the `VisitsController#create` working.

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -7,7 +7,17 @@ class VisitsController < ApplicationController
   end
 
   def create
+    @visit = Visit.new(visit_params)
 
+    if @visit.save
+      redirect_to root_path, notice: "Your visit has been recorded."
+    else
+      render :new
+    end
   end
 
+  private
+  def visit_params
+    params.require(:visit).permit(:host_id, visitor_attributes: [:first_name, :last_name])
+  end
 end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,5 +3,4 @@ class Visit < ActiveRecord::Base
   belongs_to :host
 
   accepts_nested_attributes_for :visitor
-  accepts_nested_attributes_for :host
 end
